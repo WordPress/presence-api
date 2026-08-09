e88006cfcc849af34dea32e73d0bb90ba3de046d
Joe Fusco <hello@josephfus.co>
2026-08-09 19:11:26 -0400

    test: extract shared tear_down into a WP_Presence_UnitTestCase base class
    Fixes #222

--- tests/test-post-list.php
+++ tests/test-post-list.php
@@ -11,7 +11,7 @@
  * @covers ::wp_presence_render_editors_column
  * @covers ::wp_presence_editors_column_css
  */
-class WP_Test_Presence_Post_List extends WP_UnitTestCase {
+class WP_Test_Presence_Post_List extends WP_Presence_UnitTestCase {
 
 	private static $editor_id;
 	private static $subscriber_id;
@@ -19,13 +19,6 @@ class WP_Test_Presence_Post_List extends WP_UnitTestCase {
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 		self::$editor_id     = $factory->user->create( array( 'role' => 'editor' ) );
 		self::$subscriber_id = $factory->user->create( array( 'role' => 'subscriber' ) );
-	}
-
-	public function tear_down() {
-		global $wpdb;
-		// phpcs:ignore WordPress.DB.DirectDatabaseQuery
-		$wpdb->query( "TRUNCATE TABLE {$wpdb->presence}" );
-		parent::tear_down();
 	}
 
 	/**
