ae96e1790efb19f44ddec498e84f2d6bccea4dea
Joe Fusco <hello@josephfus.co>
2026-08-16 22:29:31 -0700

    Use phpcs:disable/enable for InterpolatedNotPrepared check
    The phpcs:ignore on a single line wasn't working because the violation is on the SQL string line, not the get_results line. Using disable/enable wraps the entire block.

--- includes/functions.php
+++ includes/functions.php
@@ -776,7 +776,8 @@ function wp_presence_hydrate_room_users( $rooms, $timeout = WP_PRESENCE_DEFAULT_
 	$room_names   = wp_list_pluck( $rooms, 'room' );
 	$placeholders = implode( ', ', array_fill( 0, count( $room_names ), '%s' ) );
 
-	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.PreparedSQL.InterpolatedNotPrepared, WordPress.DB.PreparedSQLPlaceholders.UnfinishedPrepare
+	// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.PreparedSQLPlaceholders.UnfinishedPrepare
 	$rows = $wpdb->get_results(
 		$wpdb->prepare(
 			"SELECT room, user_id
@@ -785,6 +786,7 @@ function wp_presence_hydrate_room_users( $rooms, $timeout = WP_PRESENCE_DEFAULT_
 			array_merge( $room_names, array( $cutoff ) )
 		)
 	);
+	// phpcs:enable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 
 	// Group user IDs by room.
 	$room_user_ids = array();
