802c5e3e147f533ee260e66e70f5f232e1fc9719
Joe Fusco <hello@josephfus.co>
2026-09-08 09:05:05 -0400

    fix: stop PHPStan's bootstrap from silently exiting before analysis

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Constants PHPStan can't resolve by parsing presence-api.php alone.
+ *
+ * @package Presence_API
+ */
+
+define( 'WP_PRESENCE_PLUGIN_URL', 'https://example.com/wp-content/plugins/presence-api/' );
+define( 'MINUTE_IN_SECONDS', 60 );
